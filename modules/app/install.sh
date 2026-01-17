a506cad165c4a53f9854d09932340c692e5e20f5
Sitepow <kteeraphatt@gmail.com>
2026-01-16 22:22:04 -0800

    fix: postgrestsql create user fail

--- modules/app/install.sh
+++ modules/app/install.sh
@@ -91,14 +91,23 @@ sudo systemctl restart postgresql
 sudo -u postgres psql <<EOF
 DO \$\$
 BEGIN
-  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$DB_USER') THEN
+  IF EXISTS (SELECT FROM pg_roles WHERE rolname = '$DB_USER') THEN
+    ALTER ROLE $DB_USER WITH PASSWORD '$DB_PASSWORD';
+  ELSE
     CREATE ROLE $DB_USER LOGIN PASSWORD '$DB_PASSWORD';
   END IF;
+END
+\$\$;
+
+DO \$\$
+BEGIN
   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME') THEN
     CREATE DATABASE $DB_NAME OWNER $DB_USER;
   END IF;
 END
 \$\$;
+
+GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOF
 
 curl -fsSL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
